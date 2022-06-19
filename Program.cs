a5aae40358f22a37c602d049efd89b8ca6a3af18
Olga13T <Helga13v@gmail.com>
2022-06-19 13:51:26 +0300

    решение задачи в program файле

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+
+
+
+
+Console.WriteLine("Введите слова через запятую или пробел:  ");
+string str = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Укажите до какого количества символов нужно вывести из строки:  ");
+int simbol = int.Parse(Console.ReadLine());
+Console.WriteLine();
+
+void SelectWord(string text, int count)
+{
+    string[] words = text.Split(',');
+    int min = words.Select(words => words.Length).Min();
+    min = count;
+    foreach (string word in words)
+    {
+        if (word.Length <= min)
+        {
+            Console.Write(word + " ");
+        }
+    }
+}
+SelectWord(str, simbol);
